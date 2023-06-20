c3d8f76649b113d111629743c5536b7425d52660
Grzegorz Boroń <grzegorzboron23@gmail.com>
2023-06-20 17:00:25 +0200

    add buttonhandler

--- Commands.py
+++ Commands.py
@@ -1,5 +1,7 @@
 #Here is a list of functions, when called they return the array of bytes.
 # CRC needs to be added, and they can be send
+from Queue import Queue
+
 CMD_SIZE = 10
 
 def GlueCMD(arg0, arg1):
@@ -33,6 +35,21 @@ def CMD_RD_REG(RegAddr):
 
 def CMD_WR_REG(RegAddr,RegData):
     return GlueCMD(RegAddr,RegData)
+
+    def __init__(self):
+        self.queue = Queue()  # Tworzenie instancji klasy Queue
+
+    def add_command_to_queue(self, command):
+        self.queue.add_command(command)
+
+    def process_queue(self):
+        removed_command = self.queue.remove_command()
+        if removed_command:
+            print("Usunięta komenda:", removed_command)
+
+    def print_queue(self):
+        queue_string = self.queue.get_queue_as_string()
+        print("Stan kolejki:", queue_string)
 
 if __name__ == "__main__":
     #Tu testujemy komendy:
---
+++ Queue.py
@@ -0,0 +1,29 @@
+class Queue:
+    def __init__(self):
+        self.commands = []
+
+    def add_command(self, command):
+        self.commands.append(command)
+
+    def remove_command(self):
+        if self.commands:
+            return self.commands.pop(0)
+        else:
+            return None
+
+    def get_queue_as_string(self):
+        return ', '.join(self.commands)
+
+# Przykładowe użycie
+queue = Queue()
+queue.add_command("Komenda 1")
+queue.add_command("Komenda 2")
+queue.add_command("Komenda 3")
+
+print(queue.get_queue_as_string())  # Wyświetli: Komenda 1, Komenda 2, Komenda 3
+
+removed_command = queue.remove_command()
+if removed_command:
+    print("Usunięta komenda:", removed_command)  # Wyświetli: Usunięta komenda: Komenda 1
+
+print(queue.get_queue_as_string())  # Wyświetli: Komenda 2, Komenda 3
